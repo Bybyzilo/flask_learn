e930b91c84bf40d687006f75c5b5975aa4b87a23
Bybyzilo <vanyathedonkey@yandex.ru>
2025-04-09 12:23:27 +0300

    make user registration and start working on authorization

--- website/models.py
+++ website/models.py
@@ -4,15 +4,15 @@ from sqlalchemy.sql import func
 
 
 class Note(db.Model):
-    id = db.Colum(db.Integer, primary_key=True)
-    data = db.Colum(db.String(10000))
-    date = db.Colum(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Colum(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
-    id = db.Colum(db.Integeer, primary_key=True)
-    email = db.Colum(db.String(150), unic=True)
-    password = db.Colum(db.String(150))
-    first_name = db.Colun(db.String(150))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
